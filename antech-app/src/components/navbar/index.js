7cb346f9a640dc379795363bf5d18149b73c99f6
Ashraf Ali <ali4282271@gmail.com>
2023-11-30 15:17:33 +0500

    over flow hidden

--- antech-app/src/components/navbar/index.js
+++ antech-app/src/components/navbar/index.js
@@ -56,7 +56,7 @@ export default function Navbar() {
 
 
   return (
-    <header className="bg-white mx-auto overflow-hidden">
+    <header className="bg-white mx-auto ">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
